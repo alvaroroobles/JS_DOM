7cdff5274b6a5f528c780ef236437bcb4f3d5436
alvaroroobles <alvaro.becerril.robles@gmail.com>
2025-04-10 09:28:27 +0200

    Javascript para el listado de tareas

--- EJ3/myScript.js
+++ EJ3/myScript.js
@@ -2,8 +2,18 @@ const btn_task = document.getElementById("btn-task");
 const listado=document.getElementById("list");
 const input_task = document.getElementById("task");
 
-btn_task.addEventListener("click", guardar);
+btn_task.addEventListener("click", addTask);
 
-function guardar() {
-    listado.innerHTML += input_task.value + "<br>";
+function addTask() {
+    const li= document.createElement("li");
+    const p = document.createElement("p");
+    // El p va a tener el texto del input
+    p.textContent = input_task.value;
+    // El li va a tener el p
+    li.appendChild(p);
+    // El listado va a tener el li
+    listado.appendChild(li);
+
+    // Limpiamos el input
+    input_task.value = "";
 }
