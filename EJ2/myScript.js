3e6957de73ebf1075a8163094aac37cf59e08135
alvaroroobles <alvaro.becerril.robles@gmail.com>
2025-04-10 08:54:51 +0200

    hacemos todas las funciones con el addEventListener

--- EJ2/myScript.js
+++ EJ2/myScript.js
@@ -1,22 +1,45 @@
-function violet() {
-    document.getElementById("capa").style.backgroundColor = "violet";
+
+   const capa= document.getElementById("capa");
+   const boton_rojo= document.getElementById("boton_rojo");
+   const boton_amarillo= document.getElementById("boton_amarillo");
+   const boton_verde= document.getElementById("boton_verde");
+   const boton_violeta= document.getElementById("boton_violeta"); 
+   const boton_ocultar= document.getElementById("ocultar_botones");
+   const boton_mostrar= document.getElementById("mostrar_botones");
+   
+   boton_rojo.addEventListener("click",rojo);
+   boton_amarillo.addEventListener("click",amarillo);
+   boton_verde.addEventListener("click",verde);
+   boton_violeta.addEventListener("click",violeta);
+   boton_ocultar.addEventListener("click",ocultar_botones);
+   boton_mostrar.addEventListener("click",mostrar_botones);
+
+
+function rojo(){
+   capa.style.background = "red";
 }
 
+
+
 function amarillo(){
-    document.getElementById("capa").style.backgroundColor = "yellow";
-}
-function rojo(){
-    document.getElementById("capa").style.backgroundColor = "red";
-}
-function verde(){
-    document.getElementById("capa").style.backgroundColor = "green";
-}
-function ocultar_botones(){
-    document.getElementById("capa").style.visibility = "hidden";
-}
-function mostrar_botontes(){
-    document.getElementById("capa").style.visibility = "visible";
-}
+    capa.style.backgroundColor = "yellow";
+ }
+ function rojo(){
+     capa.style.backgroundColor = "red";
+ }
+ function verde(){
+    capa.style.backgroundColor = "green";
+ }
+ function ocultar_botones(){
+     capa.style.visibility = "hidden";
+ }
+ function mostrar_botones(){
+     capa.style.visibility = "visible";
+ }
 function cambiarcolor(){
-    document.getElementById("capa").style.backgroundColor = "blue";
-}
+    capa.style.backgroundColor = "blue";
+ }
+
+ function violeta(){
+     capa.style.backgroundColor = "violet";
+ }
