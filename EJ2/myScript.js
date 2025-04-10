b17d030aac4b98c1b163716d29fc46ce31878ada
alvaroroobles <alvaro.becerril.robles@gmail.com>
2025-04-10 08:27:51 +0200

    Cambiamos las funciones a un archivo externo .js

--- EJ2/myScript.js
+++ EJ2/myScript.js
@@ -1,3 +1,22 @@
 function violet() {
     document.getElementById("capa").style.backgroundColor = "violet";
 }
+
+function amarillo(){
+    document.getElementById("capa").style.backgroundColor = "yellow";
+}
+function rojo(){
+    document.getElementById("capa").style.backgroundColor = "red";
+}
+function verde(){
+    document.getElementById("capa").style.backgroundColor = "green";
+}
+function ocultar_botones(){
+    document.getElementById("capa").style.visibility = "hidden";
+}
+function mostrar_botontes(){
+    document.getElementById("capa").style.visibility = "visible";
+}
+function cambiarcolor(){
+    document.getElementById("capa").style.backgroundColor = "blue";
+}
