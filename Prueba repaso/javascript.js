2ac13bad1d5e391cbb595baabc0ba31b1a2b7724
alvaroroobles <alvaro.becerril.robles@gmail.com>
2025-05-08 09:32:19 +0200

    ejercico de toggle class con  el z-index

--- Prueba repaso/javascript.js
+++ Prueba repaso/javascript.js
@@ -4,18 +4,21 @@ const resultado = document.getElementById("resultado");
 const boton_tarea = document.getElementById("boton_tarea");
 const boton_imagen = document.getElementById("boton_imagen");
 const imagen = document.getElementById("imagen");
+const boton_z = document.getElementById("boton_z");
 
 boton.addEventListener("click", sqrt);
 boton_tarea.addEventListener("click", addTask);
 boton_imagen.addEventListener("click", toggleImagen);
+boton_z.addEventListener("click", arriba);
+
+
   // Esta función calcula la raíz cuadrada de un número
   function sqrt() {
     let numero = document.getElementById("numero").value;
     if (numero >= 0) {
       resultado.innerHTML = " Su resultado es: " + Math.sqrt(numero);
     } else {
-      resultado.innerHTML =
-        " No se puede calcular la raíz cuadrada de un número negativo";
+      resultado.innerHTML =" No se puede calcular la raíz cuadrada de un número negativo";
     }
   }
 
@@ -37,3 +40,6 @@ boton_imagen.addEventListener("click", toggleImagen);
     imagen.classList.toggle("ocultar");
   }
 
+function arriba(){
+    imagen2.classList.toggle("zindex");
+}
